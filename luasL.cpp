7da8d9ed65e659471af3e82f10db88d5e98e66f7
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-02 14:51:34 +0700

    menambahkan implementasi fungsi dan prosedur

--- luasL.cpp
+++ luasL.cpp
@@ -4,3 +4,12 @@ using namespace std;
 
 //deklarasi variabel global
 int phi, r;
+
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "masukkan phi : ";
+    cin >> phi;
+    cout << "masukkan r : ";
+    cin >> r;
+
+}
